ec419251e6d1f8ab595483a01fb02b2d56113491
Douglas <douglas.cosmo7@gmail.com>
2020-12-01 08:23:44 -0300

    organizacao dos arquivos de teste

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,15 +1,12 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Date;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Test;
+
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 public class LocacaoService {
 
@@ -29,21 +26,5 @@ public class LocacaoService {
         //TODO adicionar método para salvar
 
         return locacao;
-    }
-
-    @Test
-    public void main() {
-        //cenario
-        LocacaoService service = new LocacaoService();
-        Usuario usuario = new Usuario("Usuario1");
-        Filme filme = new Filme("filme 1", 2, 5.0);
-
-        //acão
-        Locacao locacao = service.alugarFilme(usuario, filme);
-
-        //verificaçao
-        assertTrue(locacao.getValor() == 5.0);
-        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
     }
 }
